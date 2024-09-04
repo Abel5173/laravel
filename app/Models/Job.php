f5c3474f5b11ab0b03b87ec9dff06b1813e2876a
Abel <abelzeleke5173@gmail.com>
2024-09-04 23:10:03 +0300

    Added CSRF, Form and guard fields.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Job extends Model{
     use HasFactory;
     protected $table = 'job_listings'; // This is the name of the table in the database, what it is doing here is telling Laravel that the model Job is associated with the table job_listings in the database. This is important because Laravel uses the name of the model to determine the name of the table in the database. If the table name is different from the model name, you need to specify the table name in the model. And to specify the table name, you need to use the $table property in the model.
-    protected $fillable = ['title', 'salary']; // This is an array of the columns in the table that can be mass assigned. This is important because Laravel uses mass assignment to insert data into the database.
+    // protected $fillable = ['employer_id', 'title', 'salary']; // This is an array of the columns in the table that can be mass assigned. This is important because Laravel uses mass assignment to insert data into the database.
+    protected $guarded = []; // This is an array of the columns in the table that cannot be mass assigned. This is important because Laravel uses mass assignment to insert data into the database. If a column is in the $guarded array, it cannot be mass assigned. This is important because it prevents users from inserting data into columns that they should not have access to. In this case, the $guarded array is empty, which means that all columns in the table can be mass assigned. This is not recommended because it can lead to security vulnerabilities. It is better to specify the columns that can be mass assigned using the $fillable property.
 
     public function employer() {
         return $this->belongsTo(Employer::class);
